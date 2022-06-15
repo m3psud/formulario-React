e2499a4d77bc45910c208294c929e4aeba07d150
mildred morales <mildredmoralesalmeida1503@gmail.com>
2022-06-15 11:13:44 -0300

    teste de react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ export default class myApp extends React.Component{
+     constructor(){
+      super()
 
-export default App;
+      this.state = {
+        firstName:"",
+        lastName:"",
+        age:"",
+        gender:"",
+        form:{}
+       }
+       this.handleChange = this.handleChange.bind(this);
+       this.handleSubmit= this.handleSubmit.bind(this);
+     }
+      handleChange(event){
+        const {name, value} = event. target
+       // this.setState({ [event.target.name] : event.target.value })
+        this.setState({ [name] : value})
+
+      }
+      handleSubmit(event){
+        event.preventDefault()
+
+        const {form, ...stateData} = this. state
+        this.setState({ form: stateData})
+        console.log(stateData)
+      }
+     
+  render (){
+    return (
+      <div>
+        <fom onSubmit={this.handleSubmit}>
+        <input
+          name ="firstName"
+          value={this.state.firstName}
+          onChange={this. handleChange}
+          placeholder= "First Name"
+
+        />
+        <br />
+
+        <input
+          name="lastName"
+          value={this.state.lastName}
+          onChange={this. handleChange}
+          placeholder= "Last Name"
+
+        />
+        <br />
+
+        <input
+          name="age"
+          value={this.state.age}
+          onChange={this. handleChange}
+          placeholder= "Age"
+
+        />
+
+         <label>
+          <input
+          type="radio"
+          name="gender"
+          value="male"
+          checked={this.state.gender === "male"}
+          onChange={this. handleChange}
+          />{" "}
+          Male
+         </label>
+
+         <br />
+
+         <label>
+          <input
+          type="radio"
+          name="gender"
+          value="female"
+          checked={this.state.gender === "female"}
+          onChange={this. handleChange}
+          />{" "}
+          Female
+         </label>
+
+         <br />
+          <button type="submit">Submit</button>
+
+
+        </fom>
+         <br />
+         <h2>Entered information</h2>
+         <p>
+          Your name={this.state.firstName} {this.state.lastName}
+         </p>
+
+         <p> Your age={this.state.age}  </p>
+      
+         <p> Your gender={this.state.gender}  </p>
+
+         <br />
+         <h2>Submit information</h2>
+         <p>
+          Your name={this.state.firstName} {this.state.lastName}
+         </p>
+
+         <p> Your age={this.state.age}  </p>
+      
+         <p> Your gender={this.state.gender}  </p>
+      </div>
+      
+    )
+  }
+ }
